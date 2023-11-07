833405f70b8253cc89d9cdc03e72645d1add1a28
Aldo Santos <aldoacesso@gmail.com>
2023-11-07 15:58:54 -0300

    Integre TypeScript com DOM

---
+++ src/dom.js
@@ -0,0 +1,9 @@
+"use strict";
+const campoNome = document.getElementById('nome');
+const formulario = document.getElementsByTagName('form');
+const botaoEnviar = document.getElementById('btn-enviar');
+// ? é como um escape, caso o evento seja nulo
+botaoEnviar === null || botaoEnviar === void 0 ? void 0 : botaoEnviar.addEventListener('click', (e) => {
+});
+// botaoEnviar?.addEventListener('click', (e: KeyboardEvent) => {
+// })
---
+++ src/dom.ts
@@ -0,0 +1,12 @@
+const campoNome = document.getElementById('nome');
+const formulario = document.getElementsByTagName('form');
+const botaoEnviar = document.getElementById('btn-enviar');
+
+// ? é como um escape, caso o evento seja nulo
+botaoEnviar?.addEventListener('click', (e: MouseEvent) => {
+    
+})
+
+// botaoEnviar?.addEventListener('click', (e: KeyboardEvent) => {
+
+// })
